ea23de85d1c8c19bd227b6e3cc1b4ee3879be4e8
brantou <brantou89@gmail.com>
2017-08-08 11:20:47 +0800

    Add files via upload

---
+++ jobs/jobs/items.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class JobsItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pid = scrapy.Field()
+    city = scrapy.Field()
+    education = scrapy.Field()
+    workYear = scrapy.Field()
+    salary = scrapy.Field()
+    firstType = scrapy.Field()
+    secondType = scrapy.Field()
+    positionName = scrapy.Field()
+    positionAdvantage = scrapy.Field()
+    positionLables = scrapy.Field()
+    companyFullName = scrapy.Field()
+    companyShortName = scrapy.Field()
+    companySize = scrapy.Field()
+    companyLabelList = scrapy.Field()
+    financeStage = scrapy.Field()
+    industryField = scrapy.Field()
+    industryLables = scrapy.Field()
+    district = scrapy.Field()
+    isSchoolJob = scrapy.Field()
+    jobNature = scrapy.Field()
+    keyword = scrapy.Field()
+    createTime = scrapy.Field()
---
+++ jobs/jobs/pipelines.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import pymongo
+import json
+from scrapy.exceptions import DropItem
+
+class JobsPipeline(object):
+    def process_item(self, item, spider):
+        return item
+
+class MongoPipeline(object):
+
+    collection_name = 'jobs_items'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI', 'mongodb://localhost:27017/'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'jobs')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        self.db[self.collection_name].insert_one(dict(item))
+        return item
+
+
+class DuplicatesPipeline(object):
+
+    def __init__(self):
+        self.ids_seen = set()
+
+    def process_item(self, item, spider):
+        if item['pid'] in self.ids_seen:
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            self.ids_seen.add(item['pid'])
+            return item
