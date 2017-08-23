6f6d9c91a8b2c60466b0b09f3a22fd7a62edddb2
brantou <brantou89@gmail.com>
2017-08-24 00:26:54 +0800

    Add didi and metituan spider

---
+++ jobs/jobs/spiders/didi.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+
+class DidiSpider(scrapy.Spider):
+    name = 'didi'
+    allowed_domains = ['job.didichuxing.com']
+    start_urls = ['http://job.didichuxing.com']
+    positionUrl = 'http://job.didichuxing.com/recruit/api/front/list'
+    detailUrl = 'http://job.didichuxing.com/recruit/api/front/view/%d'
+
+    headers = {
+        'accept':
+        "application/json, text/plain, */*",
+        'user-agent':
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36",
+        'dnt':
+        "1",
+        'accept-encoding':
+        "gzip, deflate",
+        'accept-language':
+        "zh-CN,zh;q=0.8,en;q=0.6",
+    }
+
+    pageIndex = 1
+    pageSize = 10
+    itemCount = 0
+
+    def start_requests(self):
+        return [self.next_request()]
+
+    def parse(self, response):
+        jdict = json.loads(response.body)
+        jdata = jdict['data']
+        self.itemCount = jdata['total']
+        items = jdata['items']
+        for item in items:
+            yield scrapy.http.FormRequest(
+                url=self.detailUrl % (item['jdId']),
+                method='GET',
+                headers=self.headers,
+                callback=self.parse_detail)
+
+        if self.pageIndex * self.pageSize < self.itemCount:
+            yield self.next_request()
+
+    def parse_detail(self, response):
+        jdict = json.loads(response.body)
+        jdata = jdict['data']
+        jdata['pid'] = response.url.split('/')[-1]
+        yield jdata
+
+    def next_request(self):
+        return scrapy.http.FormRequest(
+            url=self.positionUrl,
+            method='GET',
+            headers=self.headers,
+            formdata={
+                "channelCode": "501",
+                "page": str(self.pageIndex),
+                "recruitType": "1",
+                "size": str(self.pageSize)
+            },
+            callback=self.parse)
---
+++ jobs/jobs/spiders/meituan.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+
+
+class MeituanSpider(scrapy.Spider):
+    name = 'meituan'
+    allowed_domains = ['zhaopin.meituan.com']
+    start_urls = ['http://zhaopin.meituan.com/']
+    positionUrl = 'http://zhaopin.meituan.com/search'
+    detailUrl = 'http://zhaopin.meituan.com/%d/jobDetail'
+
+    headers = {
+        'origin':
+        "http://zhaopin.meituan.com",
+        'user-agent':
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36",
+        'content-type':
+        "application/json",
+        'accept':
+        "*/*",
+        'dnt':
+        "1",
+        'accept-encoding':
+        "gzip, deflate",
+        'accept-language':
+        "zh-CN,zh;q=0.8,en;q=0.6",
+    }
+
+    pageIndex = 1
+    pageSize = 8
+    itemCount = 0
+
+    def start_requests(self):
+        return [self.next_request()]
+
+    def parse(self, response):
+        jdict = json.loads(response.body)
+        if jdict['code'] != 200:
+            return
+
+        pageInfo = jdict['pageInfo']
+        self.itemCount = pageInfo['itemCount']
+        pageList = jdict['pageList']
+        for page in pageList:
+            yield scrapy.http.FormRequest(
+                url=self.detailUrl % (page['id']),
+                method='POST',
+                headers=self.headers,
+                callback=self.parse_detail)
+
+        self.pageIndex += 1
+        if self.pageIndex * self.pageSize < self.itemCount:
+            yield self.next_request()
+
+    def parse_detail(self, response):
+        jdict = json.loads(response.body)
+        if jdict['code'] == 200:
+            jdata = jdict['data']
+            jdata['pid'] = jdata['id']
+            yield jdata
+
+    def next_request(self):
+        payload = json.dumps({
+            "pageInfo": {
+                "pageIndex": self.pageIndex,
+                "pageSize": self.pageSize,
+                "itemCount": self.itemCount,
+            },
+            "workLocations": [],
+            "jobTypes": [],
+            "jobSubTypes": [],
+            "keyword": ""
+        })
+        print(payload)
+
+        return scrapy.http.FormRequest(
+            url=self.positionUrl,
+            method='POST',
+            headers=self.headers,
+            body=payload,
+            callback=self.parse)
