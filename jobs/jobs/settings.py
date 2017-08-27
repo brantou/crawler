fb09e9cb980a7fc27842cf3caa36461991d21072
brantou <brantou89@gmail.com>
2017-08-27 13:28:48 +0800

    adjust lagou spider

--- jobs/jobs/settings.py
+++ jobs/jobs/settings.py
@@ -21,15 +21,15 @@ NEWSPIDER_MODULE = 'jobs.spiders'
 #ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 8
+CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 8
-CONCURRENT_REQUESTS_PER_IP = 8
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
 COOKIES_ENABLED = False
--- jobs/jobs/spiders/lagou.py
+++ jobs/jobs/spiders/lagou.py
@@ -10,14 +10,11 @@ class LagouSpider(scrapy.Spider):
     start_urls = ['https://www.lagou.com/jobs/']
     positionUrl = 'https://www.lagou.com/jobs/positionAjax.json?'
 
-    totalPageCount = 0
+    totalCount = 0
+    pageSize = 0
     curPage = 1
 
     city = u'上海'
-    #keywords = [
-    #    u'大数据', u'云计算', u'docker', u'中间件', 'Node.js', u'数据挖掘', u'自然语言处理',
-    #    u'搜索算法', u'精准推荐', u'全栈工程师', u'图像处理', u'机器学习', u'语音识别'
-    #]
     keywords = [
         u'.NET',
         u'APP设计师',
@@ -302,7 +299,7 @@ class LagouSpider(scrapy.Spider):
         'x-anit-forge-code':
         "0",
         'user-agent':
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36",
         'content-type':
         "application/x-www-form-urlencoded",
         'accept':
@@ -313,18 +310,12 @@ class LagouSpider(scrapy.Spider):
         "None",
         'dnt':
         "1",
-        'referer':
-        "https://www.lagou.com/jobs/list_java?labelWords=&fromSearch=true&suginput=",
         'accept-encoding':
         "gzip, deflate, br",
         'accept-language':
         "zh-CN,zh;q=0.8,en;q=0.6",
         'cookie':
-        "user_trace_token=20170728162449-adcee15cc85848189cfb891619b80998; LGUID=20170728162451-3a327df6-736e-11e7-b9bc-5254005c3644; _gat=1; JSESSIONID=ABAAABAACBHABBI1DE966CF9357E8BD2D82C53257584955; PRE_UTM=; PRE_HOST=; PRE_SITE=https%3A%2F%2Fwww.lagou.com%2Fjobs%2Flist_golang%3FlabelWords%3D%26fromSearch%3Dtrue%26suginput%3D; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2F; index_location_city=%E5%85%A8%E5%9B%BD; TG-TRACK-CODE=index_checkmore; X_HTTP_TOKEN=2274185a2011ec03f73ab98f8ceaf490; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1501230293; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1502382038; _ga=GA1.2.1475031428.1501230295; LGSID=20170811001727-66af8955-7de7-11e7-96c3-525400f775ce; LGRID=20170811001930-b074a8af-7de7-11e7-96c3-525400f775ce; SEARCH_ID=a614dcefbf1d400780199aa2e9b1c95e; user_trace_token=20170728162449-adcee15cc85848189cfb891619b80998; LGUID=20170728162451-3a327df6-736e-11e7-b9bc-5254005c3644; user_trace_token=20170728162449-adcee15cc85848189cfb891619b80998; LGUID=20170728162451-3a327df6-736e-11e7-b9bc-5254005c3644; _gat=1; PRE_UTM=; PRE_HOST=; PRE_SITE=https%3A%2F%2Fwww.lagou.com%2Fgongsi%2F; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2F; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1501230293; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1502115517; _ga=GA1.2.1475031428.1501230295; LGSID=20170807221703-15e8d3a5-7b7b-11e7-839b-5254005c3644; LGRID=20170807221749-312e97c2-7b7b-11e7-8499-525400f775ce; index_location_city=%E4%B8%8A%E6%B5%B7; index_location_city=%E5%85%A8%E5%9B%BD; X_HTTP_TOKEN=2274185a2011ec03f73ab98f8ceaf490; JSESSIONID=ABAAABAACBHABBI1DE966CF9357E8BD2D82C53257584955; PRE_UTM=; PRE_HOST=; PRE_SITE=; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2F; TG-TRACK-CODE=index_search; _ga=GA1.2.1475031428.1501230295; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1501230293; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1502897724; LGSID=20170816233506-7a902bd5-8298-11e7-be41-525400f775ce; LGRID=20170816233523-84dd2b0e-8298-11e7-be41-525400f775ce; SEARCH_ID=b1bfc38325524b19834cbce76deddaa6",
-        'cache-control':
-        "no-cache",
-        'postman-token':
-        "fdd777d6-9b34-b322-5c2d-c6164023e669"
+        "user_trace_token=20170728162449-adcee15cc85848189cfb891619b80998; LGUID=20170728162451-3a327df6-736e-11e7-b9bc-5254005c3644; _gat=1; PRE_UTM=; PRE_HOST=; PRE_SITE=https%3A%2F%2Fwww.lagou.com%2Fgongsi%2F; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2F; LGSID=20170807221703-15e8d3a5-7b7b-11e7-839b-5254005c3644; index_location_city=%E5%85%A8%E5%9B%BD; JSESSIONID=ABAAABAACBHABBI1DE966CF9357E8BD2D82C53257584955; X_HTTP_TOKEN=2274185a2011ec03f73ab98f8ceaf490; TG-TRACK-CODE=index_navigation; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1501230293; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1503803797; _ga=GA1.2.1475031428.1501230295; LGRID=20170827111636-22b244c2-8ad6-11e7-8f3c-5254005c3644; SEARCH_ID=52d0e756ac5d41b395d4a9e57ab74b72; user_trace_token=20170728162449-adcee15cc85848189cfb891619b80998; LGUID=20170728162451-3a327df6-736e-11e7-b9bc-5254005c3644; index_location_city=%E5%85%A8%E5%9B%BD; user_trace_token=20170728162449-adcee15cc85848189cfb891619b80998; LGUID=20170728162451-3a327df6-736e-11e7-b9bc-5254005c3644; _gat=1; PRE_UTM=; PRE_HOST=; PRE_SITE=https%3A%2F%2Fwww.lagou.com%2Fgongsi%2F; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2F; LGSID=20170807221703-15e8d3a5-7b7b-11e7-839b-5254005c3644; index_location_city=%E5%85%A8%E5%9B%BD; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1501230293; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1503587852; _ga=GA1.2.1475031428.1501230295; LGRID=20170824231731-59096c0b-88df-11e7-8ea0-5254005c3644; JSESSIONID=ABAAABAACBHABBI1DE966CF9357E8BD2D82C53257584955; X_HTTP_TOKEN=2274185a2011ec03f73ab98f8ceaf490; TG-TRACK-CODE=index_navigation; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1501230293; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1503807054; _ga=GA1.2.1475031428.1501230295; LGSID=20170827121054-b83e61a1-8add-11e7-8f3c-5254005c3644; PRE_UTM=; PRE_HOST=; PRE_SITE=https%3A%2F%2Fwww.lagou.com%2Fzhaopin%2Fheiheceshi%2F%3FlabelWords%3Dlabel; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2Fjobs%2Flist_Go%3Fcity%3D%25E5%2585%25A8%25E5%259B%25BD%26cl%3Dfalse%26fromSearch%3Dtrue%26labelWords%3D%26suginput%3D; LGRID=20170827121054-b83e632d-8add-11e7-8f3c-5254005c3644; SEARCH_ID=ced77b843c064940a3c12c51d8720ca1",
     }
 
     def start_requests(self):
@@ -335,13 +326,16 @@ class LagouSpider(scrapy.Spider):
         jcontent = jdict['content']
         jposresult = jcontent['positionResult']
         jresult = jposresult['result']
-        self.totalPageCount = int(jposresult['totalCount']) / int(
-            jcontent['pageSize']) + 1
+        self.totalCount = int(jposresult['totalCount'])
+        self.pageSize = int(jcontent['pageSize'])
+        print('[lagou][%s]totalCount: %d, pageNo: %d, pageSize: %d' %
+              (self.keyword, int(jposresult['totalCount']), self.curPage,
+               int(jcontent['pageSize'])))
         for entry in jresult:
             if len(entry) < 10:
                 continue
             item = JobsItem()
-            item['pid'] = self.keyword + str(entry['positionId']) + "_" + str(
+            item['pid'] = str(entry['positionId']) + "_" + str(
                 entry['publisherId'])
             item['keyword'] = self.keyword
             for fn in self.item_fns:
@@ -349,12 +343,13 @@ class LagouSpider(scrapy.Spider):
 
             yield item
 
-        if self.curPage <= self.totalPageCount:
+        if self.curPage * self.pageSize < self.total:
             self.curPage += 1
             yield self.next_request()
         elif self.kd_cur < len(self.keywords) - 1:
             self.curPage = 1
-            self.totalPageCount = 0
+            self.pageSize = 0
+            self.totalCount = 0
             self.kd_cur += 1
             self.keyword = self.keywords[self.kd_cur]
             yield self.next_request()
