877fe8ea33dcc8996c6c5fa693fb142e439e80ca
brantou <brantou89@gmail.com>
2017-08-30 00:35:32 +0800

    Update alibaba.py

--- jobs/jobs/spiders/alibaba.py
+++ jobs/jobs/spiders/alibaba.py
@@ -29,13 +29,7 @@ class AlibabaSpider(scrapy.Spider):
         'accept-encoding':
         "gzip, deflate, br",
         'accept-language':
-        "zh-CN,zh;q=0.8,en;q=0.6",
-        'cookie':
-        "t=529c8a503c4336e01b42feef6f83d119; l=AnFxKAqEPjdNM3MIITAEY4zxAfYL2eXQ; JSESSIONID=EF6YIY4TL2-7GCNTAN0RH2ADYMMYZPO1-GWINI66J-RC1; v=0; cookie2=10162fd762f07ec9868258352ec3b968; _tb_token_=38e8f4131885e; cna=1OZ3DwF+cCECAbSnFDrpAFUK; _hvn_login=\"0,3\"; csg=0621b2b2; tmp0=A8VbiiV6ijcmBnjzDMLAxE%2FtS8iw7Ec3Y2YidZMZnatOZrndjC1dQxWsjSK2ga342M5K0mnkkshYcNBo0yCujiGkEsG6mUZ%2FyU5hV4R0yvvwrHubcTekAR8hMSGmMPE56sC9Y0v48PZ0ODXG36UR5VuPFWLflgZESq0TJ4a4z0CcmyhlQNCArRk3ik4zIj%2B5EgaQ%2FEyDiCKKzVtFBCDikTJpHkHMBUMbrYzWTz3%2BO2vShVV1Xml8ZaJoL7EEc7T3oekuv87k75y1k79tgvIyLw%3D%3D; isg=Are3WpCaohZAUCYJslICxM7fRq0LcIfUpu2mIwlkVgbtuNT6EExmL2XY5C4d; t=529c8a503c4336e01b42feef6f83d119; l=AnFxKAqEPjdNM3MIITAEY4zxAfYL2eXQ; JSESSIONID=EF6YIY4TL2-7GCNTAN0RH2ADYMMYZPO1-GWINI66J-RC1; v=0; cookie2=10162fd762f07ec9868258352ec3b968; _tb_token_=38e8f4131885e; cna=1OZ3DwF+cCECAbSnFDrpAFUK; _hvn_login=\"0,3\"; csg=0621b2b2; tmp0=A8VbiiV6ijcmBnjzDMLAxE%2FtS8iw7Ec3Y2YidZMZnatOZrndjC1dQxWsjSK2ga342M5K0mnkkshYcNBo0yCujiGkEsG6mUZ%2FyU5hV4R0yvvwrHubcTekAR8hMSGmMPE56sC9Y0v48PZ0ODXG36UR5VuPFWLflgZESq0TJ4a4z0CcmyhlQNCArRk3ik4zIj%2B5EgaQ%2FEyDiCKKzVtFBCDikTJpHkHMBUMbrYzWTz3%2BO2vShVV1Xml8ZaJoL7EEc7T3oekuv87k75y1k79tgvIyLw%3D%3D; isg=Are3WpCaohZAUCYJslICxM7fRq0LcIfUpu2mIwlkVgbtuNT6EExmL2XY5C4d",
-        'cache-control':
-        "no-cache",
-        'postman-token':
-        "cdec794f-9f94-ffb0-ef52-a66a06c561b9"
+        "zh-CN,zh;q=0.8,en;q=0.6"
     }
 
     def start_requests(self):
